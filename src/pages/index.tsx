b9aeea40b811f9d2b2e7f5f56d4360934198f2ac
Aryan Prince <aryanprincesanty@icloud.com>
2023-10-24 18:04:03 +0100

    chore: Updated background gradient

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main
-        className={`${inter.className} flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]`}
+        className={`${inter.className} flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-neutral-950 to-neutral-900`}
       >
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
