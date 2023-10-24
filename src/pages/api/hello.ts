247c39faaa920ab3caccb11dca7ff2338cbad44e
Aryan Prince <aryanprincesanty@icloud.com>
2023-10-24 18:17:34 +0100

    feat: Added 2 `next-api` endpoints

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Person = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Person>,
+) {
+  res.status(200).json({ name: "John Doe" });
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <main
         className={`${inter.className} flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-neutral-950 to-neutral-900`}
       >
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Welcome to UniTrack
           </h1>
