a8acbb2fc8d82757398975c5fb47768e88a1fae5
Aryan Prince <aryanprincesanty@icloud.com>
2023-10-29 01:44:35 +0000

    Added a `tRPC` router for Student table mutations

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
 import { postRouter } from "~/server/api/routers/post";
+import { studentRouter } from "~/server/api/routers/student";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  student: studentRouter,
 });
 
 // export type definition of API
