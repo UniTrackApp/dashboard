266a41b090b51ef6e0d3343ddeb27c2e7ca2e3fc
Aryan Prince <aryanprincesanty@icloud.com>
2023-10-30 03:11:21 +0000

    Uncommenting to fix Vercel build issues

--- src/pages/check-student.tsx
+++ src/pages/check-student.tsx
@@ -1,27 +1,27 @@
-// import React, { useState } from "react";
-// import { Button } from "~/components/ui/button";
-// import { Input } from "~/components/ui/input";
-// import { api } from "~/utils/api";
+import React, { useState } from "react";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { api } from "~/utils/api";
 
-// function CheckStudent() {
-//   const [student, setStudent] = useState("SX69");
+function CheckStudent() {
+  const [student, setStudent] = useState("SX69");
 
-//   const { data: fetchedStudentInfo } =
-//     api.student.getStudentById.useQuery(student);
+  const { data: fetchedStudentInfo } =
+    api.student.getStudentById.useQuery(student);
 
-//   console.log(fetchedStudentInfo);
+  console.log(fetchedStudentInfo);
 
-//   return (
-//     <div className="p-4">
-//       <Input
-//         placeholder="Enter Student UID"
-//         onChange={(e) => {
-//           setStudent(e.target.value);
-//         }}
-//       />
-//       <Button>Check Student</Button>
-//     </div>
-//   );
-// }
+  return (
+    <div className="p-4">
+      <Input
+        placeholder="Enter Student UID"
+        onChange={(e) => {
+          setStudent(e.target.value);
+        }}
+      />
+      <Button>Check Student</Button>
+    </div>
+  );
+}
 
-// export default CheckStudent;
+export default CheckStudent;
