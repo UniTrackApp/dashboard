04fcabbbf7511bff640f8c0860deff196884a323
Aryan Prince <aryanprincesanty@icloud.com>
2023-10-30 02:51:29 +0000

    Updated home page UI design Close Improve the landing page UI design #13

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,179 @@
-import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import { ArrowRight } from "lucide-react";
+import { type Session } from "next-auth";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { Button, buttonVariants } from "~/components/ui/button";
 import { inter } from "~/styles/fonts";
 import { api } from "~/utils/api";
 
-type MessageFromFlask = {
-  message: string;
-};
-
 export default function Home() {
+  const { data: sessionData } = useSession();
+
+  return (
+    <>
+      <Head>
+        <title>UniTrack</title>
+        <meta
+          name="description"
+          content="A student monitoring system for the modern universities."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main
+        className={`${inter.className} flex min-h-screen flex-col items-center justify-between bg-gradient-to-b from-neutral-950 to-neutral-900 text-white selection:bg-violet-600 selection:text-white`}
+      >
+        <Navbar sessionData={sessionData} />
+        <div className="container flex w-full flex-col items-center justify-center gap-12">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-center text-3xl font-extrabold tracking-tight sm:text-7xl">
+              Welcome to UniTrack
+            </h1>
+            <p className="text-center text-lg text-neutral-300 sm:text-xl">
+              An open-source student monitoring system for modern universities.
+            </p>
+          </div>
+
+          <Link href="/dashboard" className={`${buttonVariants()} gap-2`}>
+            View Dashboard
+            <ArrowRight size={16} />
+          </Link>
+        </div>
+        <Footer />
+      </main>
+    </>
+  );
+}
+
+// Unused function, part of the NextAuth.js showcase
+function AuthShowcase() {
+  const { data: sessionData } = useSession();
+
+  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined },
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      {sessionData?.user.image && (
+        <Avatar>
+          <AvatarImage src={sessionData?.user?.image} />
+          <AvatarFallback>DP</AvatarFallback>
+        </Avatar>
+      )}
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+}
+
+function Navbar({ sessionData }: { sessionData: Session | null }) {
+  return (
+    <>
+      <nav className="flex w-full justify-between p-6">
+        <Image
+          src={"/logo.png"}
+          width={40}
+          height={40}
+          className=""
+          alt="UniTrack Logo"
+        />
+
+        {/* WHEN SIGNED OUT */}
+        {!sessionData && (
+          <Button variant={"secondary"} onClick={() => signIn()}>
+            Sign in
+          </Button>
+        )}
+
+        {/* WHEN SIGNED IN */}
+        {sessionData && (
+          <div className="flex flex-row gap-4">
+            <div className="flex flex-col">
+              {sessionData && (
+                <span className="text-md">Hi, {sessionData.user?.name}</span>
+              )}
+
+              <Button
+                className="h-4 text-sm text-neutral-500"
+                variant={"link"}
+                size={"sm"}
+                onClick={() => signOut()}
+              >
+                Sign out
+              </Button>
+            </div>
+
+            {sessionData?.user.image && (
+              <Avatar>
+                <AvatarImage src={sessionData?.user?.image} />
+                <AvatarFallback>DP</AvatarFallback>
+              </Avatar>
+            )}
+          </div>
+        )}
+      </nav>
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <>
+      <footer className="mb-4 text-neutral-300">
+        Built with &lt;3 by{" "}
+        <Link
+          referrerPolicy="no-referrer"
+          target="_blank"
+          className="underline-offset-4 hover:underline"
+          href="https://github.com/aryanprince"
+        >
+          Aryan
+        </Link>
+        {", "}
+        <Link
+          referrerPolicy="no-referrer"
+          target="_blank"
+          className="underline-offset-4 hover:underline"
+          href="https://github.com/aryanprince"
+        >
+          Andrea
+        </Link>
+        {" and "}
+        <Link
+          referrerPolicy="no-referrer"
+          target="_blank"
+          className="underline-offset-4 hover:underline"
+          href="https://github.com/aryanprince"
+        >
+          Lewis
+        </Link>
+      </footer>
+    </>
+  );
+}
+
+// Test function used to test the Flask API integration
+function FlaskTest() {
+  type MessageFromFlask = {
+    message: string;
+  };
+
   const [message, setMessage] = useState("");
 
   useEffect(() => {
@@ -28,50 +192,7 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Create T3 App</title>
-        <meta name="description" content="Generated by create-t3-app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main
-        className={`${inter.className} flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-neutral-950 to-neutral-900`}
-      >
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Welcome to UniTrack
-          </h1>
-
-          <p className="text-white">Current message: {message}</p>
-
-          <div className="flex flex-col items-center gap-2">
-            <AuthShowcase />
-          </div>
-        </div>
-      </main>
+      <p>Current message: {message}</p>
     </>
-  );
-}
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
   );
 }
