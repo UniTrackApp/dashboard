326cbc40cbadfe9f89360b2e51c5f925c110f557
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-02 21:47:54 +0000

    feat: Added the Root Layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "UniTrack",
+  description: "Welcome to UniTrack",
+};
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
