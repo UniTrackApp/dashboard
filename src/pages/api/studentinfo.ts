ee2fddece31abacee393faea57a4f8a5514b3b28
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2023-11-03 08:59:16 +0000

    feat: UID set as unique and set Invalid request

--- src/pages/api/studentinfo.ts
+++ src/pages/api/studentinfo.ts
@@ -1,22 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { string } from "zod";
 
 import { db } from "~/server/db";
 
-export default async (request: NextApiRequest, response: NextApiResponse) => {
+export const handler = async (
+  request: NextApiRequest,
+  response: NextApiResponse,
+) => {
   if (request.method == "GET") {
     const uid = request.query.uid as string;
 
     try {
-      const result = await db.student.findFirst({
+      const result = await db.student.findUnique({
         where: {
           studentCardId: uid,
         },
+        select: {
+          studentCardId: true,
+        },
       });
       response.status(200).json(result);
-    } catch (error) {
-      response.status(500).json({ error: "Server Error" });
+    } catch (PrismaClientValidationError) {
+      response.status(404).json({ error: "Necessary field empty" });
     }
   } else {
     response.status(405).end;
   }
 };
+
+export default handler;
