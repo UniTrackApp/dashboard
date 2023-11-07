38a5a5b540a8f9c9d54cb4324f37823bf4e76701
A. La Fauci <87176210+Bosurgi@users.noreply.github.com>
2023-11-07 18:11:07 +0000

    remove note to team
    Co-authored-by: Aryan Prince <hello@aryanprince.com>

--- src/server/api/routers/attendance-records.ts
+++ src/server/api/routers/attendance-records.ts
@@ -1,7 +1,6 @@
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const attendanceRecordRouter = createTRPCRouter({
-  // Note to team: I'm naming this variable whateverProps so I can show you how destructuring works later
   getAttendanceRecordCount: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.db.attendanceRecord.count();
   }),
