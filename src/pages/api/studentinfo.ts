d2c1f67ebf274f9da6ff201b78ec77ad719c49b0
A. La Fauci <87176210+Bosurgi@users.noreply.github.com>
2023-11-07 17:45:54 +0000

    feat: API routes implemented (#44)
    * feat: UID set as unique and set Invalid request
    
    * chore: Removed unused import
    
    * feat: Error check bad response status implemented
    
    * feat: Added new database migration
    
    ---------
    
    Co-authored-by: Aryan Prince <aryanprincesanty@icloud.com>

--- src/pages/api/studentinfo.ts
+++ src/pages/api/studentinfo.ts
@@ -9,17 +9,53 @@ export const handler = async (
   if (request.method == "GET") {
     const uid = request.query.uid as string;
 
+    // Check if the UID is empty or null and returns Bad Request error
+    if (uid === "") {
+      response.status(400).json({
+        error: "Bad request - UID needs to be a string",
+      });
+      return;
+    } else if (!uid) {
+      response.status(400).json({
+        error: "Bad request - UID parameter needed",
+      });
+      return;
+    }
+
+    // Checking if the student is in database
     try {
-      const result = await db.student.findFirst({
+      const result = await db.student.findUnique({
         where: {
-          studentCardId: uid,
+          // Sanitizing the UID
+          studentCardId: uid.toUpperCase().trim(),
+        },
+        select: {
+          studentCardId: true,
         },
       });
-      response.status(200).json(result);
-    } catch (error) {
-      response.status(500).json({ error: "Server Error" });
+
+      // If student is not in DB send Bad Request
+      if (result === null) {
+        response.status(400).json({
+          error: "Bad request - Student does not exist",
+          ledStatus: "Red",
+        });
+      } else {
+        // Sending student data and 200 response
+        response.status(200).json({
+          message: "Student found",
+          data: result,
+          ledStatus: "Green",
+        });
+      }
+
+      // Prisma error handling
+    } catch (PrismaClientValidationError) {
+      response.status(404).json({ error: "Page not found" });
     }
   } else {
     response.status(405);
   }
 };
+
+export default handler;
