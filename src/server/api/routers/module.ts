ecc34ab9562e993ea42e67e41eef4f91e1887d4b
A. La Fauci <87176210+Bosurgi@users.noreply.github.com>
2023-11-07 18:11:42 +0000

    remove note to team
    Co-authored-by: Aryan Prince <hello@aryanprince.com>

--- src/server/api/routers/module.ts
+++ src/server/api/routers/module.ts
@@ -1,7 +1,6 @@
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const moduleRouter = createTRPCRouter({
-  // Note to team: I'm naming this variable whateverProps so I can show you how destructuring works later
   getModuleCount: protectedProcedure.query(async ({ctx}) => {
     return await ctx.db.module.count();
   }),
