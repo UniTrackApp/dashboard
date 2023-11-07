dfb36e3e673b192ef7fef85afc702675c36c32ee
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2023-11-07 17:17:56 +0000

    feat: Error check bad response status implemented

--- src/pages/api/studentinfo.ts
+++ src/pages/api/studentinfo.ts
@@ -9,18 +9,49 @@ export const handler = async (
   if (request.method == "GET") {
     const uid = request.query.uid as string;
 
+    // Check if the UID is empty or null and returns Bad Request error
+    if (uid === "") {
+      response.status(400).json({
+        error: "Bad request - UID needs to be a string",
+      });
+      return;
+    } else if (!uid) {
+      response.status(400).json({
+        error: "Bad request - UID parameter needed",
+      });
+      return;
+    }
+
+    // Checking if the student is in database
     try {
       const result = await db.student.findUnique({
         where: {
-          studentCardId: uid,
+          // Sanitizing the UID
+          studentCardId: uid.toUpperCase().trim(),
         },
         select: {
           studentCardId: true,
         },
       });
-      response.status(200).json(result);
+
+      // If student is not in DB send Bad Request
+      if (result === null) {
+        response.status(400).json({
+          error: "Bad request - Student does not exist",
+          ledStatus: "Red",
+        });
+      } else {
+        // Sending student data and 200 response
+        response.status(200).json({
+          message: "Student found",
+          data: result,
+          ledStatus: "Green",
+        });
+      }
+
+      // Prisma error handling
     } catch (PrismaClientValidationError) {
-      response.status(404).json({ error: "Necessary field empty" });
+      response.status(404).json({ error: "Page not found" });
     }
   } else {
     response.status(405);
