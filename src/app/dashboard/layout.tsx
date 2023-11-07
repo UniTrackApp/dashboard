be5f79b1b6b5e99f78d709832ed464b20ac0838d
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-07 23:23:20 +0000

    feat: Improved mobile responsiveness

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,18 @@
 import { type ReactNode } from "react";
 import Sidebar from "~/app/dashboard/sidebar";
+import MobileNavBar from "~/components/mobile-navbar";
 
-function DashboardLayout({ children }: { children: ReactNode }) {
+export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="grid w-full grid-cols-5 divide-x bg-neutral-100 dark:bg-neutral-800">
-      <div className="sticky top-0 col-span-1 self-start">
+    <div className="grid w-full grid-cols-4 bg-neutral-100 dark:bg-neutral-800 xl:grid-cols-5">
+      <div className="sticky top-0 col-span-1 hidden self-start md:block">
         <Sidebar />
       </div>
-      <div className="col-span-4">
-        <div className="flex flex-col gap-4 pt-4 mx-8">
+      <div className="absolute right-0 top-0 md:hidden">
+        <MobileNavBar />
+      </div>
+      <div className="col-span-full md:col-span-3 xl:col-span-4">
+        <div className="mx-8 flex flex-col gap-4 pt-4">
           <p className="text-2xl font-semibold">Dashboard</p>
           {children}
         </div>
@@ -16,5 +20,3 @@ function DashboardLayout({ children }: { children: ReactNode }) {
     </div>
   );
 }
-
-export default DashboardLayout;
---
+++ src/components/mobile-navbar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Menu } from "lucide-react";
+import { useState } from "react";
+
+export default function MobileNavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <div>
+        <button
+          onClick={() => {
+            setIsOpen(!isOpen);
+            alert(
+              "Message from Aryan: Hi, we don't have a mobile navbar yet. Please move around by changing the URL for now. Thanks.",
+            );
+          }}
+        >
+          <Menu className="m-4 h-8 w-8" />
+        </button>
+      </div>
+    </>
+  );
+}
