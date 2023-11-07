7b4a354466df7121456e97782cc6d54c6aa2ab7f
A. La Fauci <87176210+Bosurgi@users.noreply.github.com>
2023-11-07 18:11:30 +0000

    remove note to team
    Co-authored-by: Aryan Prince <hello@aryanprince.com>

--- src/server/api/routers/lecture.ts
+++ src/server/api/routers/lecture.ts
@@ -1,7 +1,6 @@
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const lectureRouter = createTRPCRouter({
-  // Note to team: I'm naming this variable whateverProps so I can show you how destructuring works later
   getLectureCount: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.db.lecture.count();
   }),
