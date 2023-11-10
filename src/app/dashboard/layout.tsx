8c6d614e48dc89829c5bbdd66e15a1ecd46ca460
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-10 15:04:57 +0000

    refactor: Moved quick stats to dashboard `/home`

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -26,39 +26,6 @@ export default async function DashboardLayout({
       <div className="col-span-full md:col-span-3 xl:col-span-4">
         <div className="mx-8 flex flex-col gap-4 pt-4">
           <p className="text-2xl font-semibold">Dashboard</p>
-          <div>
-            <p className="text-lg font-medium">Quick Stats</p>
-            <div className="mt-4 flex flex-1 flex-wrap gap-4 md:flex-nowrap">
-              <Card className="" decoration="top" decorationColor="indigo">
-                <Text className="mb-2 flex items-center">
-                  <Users size={16} className="mr-2" />
-                  Students
-                </Text>
-                <Metric>{studentCount}</Metric>
-              </Card>
-              <Card className="" decoration="top" decorationColor="indigo">
-                <Text className="mb-2 flex items-center">
-                  <Clock size={16} className="mr-2" />
-                  Lectures
-                </Text>
-                <Metric>{lectureCount}</Metric>
-              </Card>
-              <Card className="" decoration="top" decorationColor="indigo">
-                <Text className="mb-2 flex items-center">
-                  <BookCopy size={16} className="mr-2" />
-                  Modules
-                </Text>
-                <Metric>{moduleCount}</Metric>
-              </Card>
-              <Card className="" decoration="top" decorationColor="indigo">
-                <Text className="mb-2 flex items-center">
-                  <CopyCheck size={16} className="mr-2" />
-                  Records
-                </Text>
-                <Metric>{attendanceRecordCount}</Metric>
-              </Card>
-            </div>
-          </div>
           {children}
         </div>
       </div>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,47 @@
-export default function Dashboard() {
+import { db } from "~/server/db";
+
+import { Card, Metric, Text } from "@tremor/react";
+import { BookCopy, Clock, CopyCheck, Users } from "lucide-react";
+
+export default async function Dashboard() {
+  const studentCount = await db.student.count();
+  const lectureCount = await db.lecture.count();
+  const moduleCount = await db.module.count();
+  const attendanceRecordCount = await db.attendanceRecord.count();
+
   return (
-    <>
-      <p>Homepage</p>
-    </>
+    <div>
+      <p className="text-lg font-medium">Quick Stats</p>
+      <div className="mt-4 flex flex-1 flex-wrap gap-4 md:flex-nowrap">
+        <Card decoration="top" decorationColor="indigo">
+          <Text className="mb-2 flex items-center">
+            <Users size={16} className="mr-2" />
+            Students
+          </Text>
+          <Metric>{studentCount}</Metric>
+        </Card>
+        <Card decoration="top" decorationColor="indigo">
+          <Text className="mb-2 flex items-center">
+            <Clock size={16} className="mr-2" />
+            Lectures
+          </Text>
+          <Metric>{lectureCount}</Metric>
+        </Card>
+        <Card decoration="top" decorationColor="indigo">
+          <Text className="mb-2 flex items-center">
+            <BookCopy size={16} className="mr-2" />
+            Modules
+          </Text>
+          <Metric>{moduleCount}</Metric>
+        </Card>
+        <Card decoration="top" decorationColor="indigo">
+          <Text className="mb-2 flex items-center">
+            <CopyCheck size={16} className="mr-2" />
+            Records
+          </Text>
+          <Metric>{attendanceRecordCount}</Metric>
+        </Card>
+      </div>
+    </div>
   );
 }
