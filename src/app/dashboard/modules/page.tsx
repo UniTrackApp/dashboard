bb020e50328aabfa540942efa572bbb6448cd9b5
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-10 22:22:06 +0000

    wip: Temporary code to test things out

--- src/app/dashboard/modules/page.tsx
+++ src/app/dashboard/modules/page.tsx
@@ -1,3 +1,135 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { toast } from "@/components/ui/use-toast";
+import { api } from "~/utils/api";
+import { cn } from "~/utils/shadcn";
+
+const FormSchema = z.object({
+  studentId: z.string(),
+});
+
 export default function Modules() {
-  return <div>Modules</div>;
+  return (
+    <>
+      <p>Hi</p>
+      {/* <StudentIDSelector /> */}
+    </>
+  );
 }
+
+export function StudentIDSelector() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  const { data: allStudents } = api.student.getAllStudents.useQuery();
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="studentId"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Student Select</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      className={cn(
+                        "w-[200px] justify-between",
+                        !field.value && "text-muted-foreground",
+                      )}
+                    >
+                      {field.value
+                        ? allStudents?.find(
+                            (student) => student.studentId === field.value,
+                          )?.studentId
+                        : "Select student ID"}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] p-0">
+                  <Command>
+                    <CommandInput placeholder="Search student IDs..." />
+                    <CommandEmpty>No student ID found.</CommandEmpty>
+                    <CommandGroup>
+                      {allStudents?.map((student) => (
+                        <CommandItem
+                          value={student.studentId}
+                          key={student.studentId}
+                          onSelect={() => {
+                            form.setValue("studentId", student.studentId);
+                          }}
+                        >
+                          {student.studentId}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+              <FormDescription>This is the description.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
+
+// {
+//   allModules?.map((module) => (
+//     <div key={module.moduleId}>
+//       {module.moduleId}
+//       {module.moduleName}
+//       {module.moduleDesc}
+//     </div>
+//   ));
+// }
