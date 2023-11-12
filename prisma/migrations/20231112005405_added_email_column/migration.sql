410fab877a0ed8e664d7c519962941bb118d59f7
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-12 00:58:00 +0000

    chore: Tested migrations with Prisma

---
+++ prisma/migrations/20231112005405_added_email_column/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Student" ADD COLUMN     "email" TEXT;
---
+++ prisma/migrations/20231112005513_removed_email_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `Student` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Student" DROP COLUMN "email";
