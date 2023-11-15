46693bf524e32e8c787b06a7be8f4ca3f109f6fa
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-15 17:57:47 +0000

    feat: Can create and view modules

--- src/app/dashboard/modules/page.tsx
+++ src/app/dashboard/modules/page.tsx
@@ -1,7 +1,78 @@
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { api } from "@/utils/api";
+
+type FormData = {
+  moduleId: string;
+  moduleName: string;
+  moduleDescription: string;
+};
+
 export default function Modules() {
+  const [formData, setFormData] = useState<FormData>({
+    moduleId: "",
+    moduleName: "",
+    moduleDescription: "",
+  });
+  const { data, refetch } = api.module.getAllModules.useQuery();
+  const { mutate } = api.module.createNewModule.useMutation();
+
   return (
     <>
-      <p>Modules</p>
+      <p>Add a new module</p>
+
+      {/* FORM */}
+      <div>
+        <Label>Module ID</Label>
+        <Input
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              moduleId: e.target.value,
+            })
+          }
+        />
+        <Label>Module Name</Label>
+        <Input
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              moduleName: e.target.value,
+            })
+          }
+        />
+        <Label>Module Description</Label>
+        <Input
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              moduleDescription: e.target.value,
+            })
+          }
+        />
+        <Button
+          onClick={() => {
+            mutate(formData);
+            void refetch();
+          }}
+        >
+          Submit
+        </Button>
+      </div>
+
+      {/* DISPLAYING DATA */}
+      {data?.map((item) => (
+        <div key={item.moduleId}>
+          {item.moduleId}
+          {item.moduleName}
+          {item.moduleDesc}
+        </div>
+      ))}
     </>
   );
 }
--- src/server/api/routers/module.ts
+++ src/server/api/routers/module.ts
@@ -1,6 +1,18 @@
+import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
+// This is a tRPC router. A router is a collection of various tRPC endpoints or 'procedures' as they call it.
+// We could technically have all our tRPC endpoints/procedures in a single router, but we use multiple routers purely for seperation of concerns.
+// So one router to manage API calls to Module table while another router manages the API calls to Attendance Record table
+// A procedure can be used to do 2 main things, either query some data or mutate some data somewhere
 export const moduleRouter = createTRPCRouter({
+  // This here is a procedure used to query the database and return some data
+  // The frontend can 'call' this API endpoint/procedure to fetch this data from backend
+
+  // 👇
+  // Now we could have this EXACT SAME code in /pages/api like we do for the client
+  // Benefit of using tRPC is that the frontend and backend can share datatypes using power of TypeScript
+  // As we'll see soon, we'll need to do more work to parse request body etc for the 'create' endpoint we're creating in /pages/api for the client rn
   getModuleCount: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.db.module.count();
   }),
@@ -8,4 +20,22 @@ export const moduleRouter = createTRPCRouter({
   getAllModules: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.db.module.findMany();
   }),
+
+  createNewModule: protectedProcedure
+    .input(
+      z.object({
+        moduleId: z.string().min(1),
+        moduleName: z.string().min(1),
+        moduleDescription: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.module.create({
+        data: {
+          moduleId: input.moduleId,
+          moduleName: input.moduleName,
+          moduleDesc: input.moduleDescription,
+        },
+      });
+    }),
 });
