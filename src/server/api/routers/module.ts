c2420660d0129ee825f754210a3ac2733a45b534
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-21 19:38:47 +0000

    docs: Added comments explaining tRPC procedures

--- src/server/api/routers/module.ts
+++ src/server/api/routers/module.ts
@@ -1,19 +1,11 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
-// This is a tRPC router. A router is a collection of various tRPC endpoints or 'procedures' as they call it.
-// We could technically have all our tRPC endpoints/procedures in a single router, but we use multiple routers purely for seperation of concerns.
-// So one router to manage API calls to Module table while another router manages the API calls to Attendance Record table
-// A procedure can be used to do 2 main things, either query some data or mutate some data somewhere
+// This is a tRPC router used to manage API calls to the Module table
 export const moduleRouter = createTRPCRouter({
-  // This here is a procedure used to query the database and return some data
-  // The frontend can 'call' this API endpoint/procedure to fetch this data from backend
-
-  // 👇
-  // Now we could have this EXACT SAME code in /pages/api like we do for the client
-  // Benefit of using tRPC is that the frontend and backend can share datatypes using power of TypeScript
-  // As we'll see soon, we'll need to do more work to parse request body etc for the 'create' endpoint we're creating in /pages/api for the client rn
+  // CREATE: Creates a new module entry in the database
   createModule: protectedProcedure
+    // Takes in an input object that is validated using zod
     .input(
       z.object({
         moduleId: z
@@ -24,6 +16,7 @@ export const moduleRouter = createTRPCRouter({
         moduleDesc: z.string().min(1).nullish(), // nullish means it can be null or undefined
       }),
     )
+    // This mutation function takes in the input object and creates a new entry in the database
     .mutation(async ({ ctx, input }) => {
       return await ctx.db.module.create({
         data: {
@@ -34,20 +27,25 @@ export const moduleRouter = createTRPCRouter({
       });
     }),
 
+  // GET: Gets only the number of module entries
   getModuleCount: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.db.module.count();
   }),
 
+  // GET: Gets all rows and columns from the module table
   getAllModules: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.db.module.findMany();
   }),
 
+  // DELETE: Deletes a module entry by its ID (primary key)
   deleteModuleById: protectedProcedure
+    // Takes in an input object that is validated using zod
     .input(
       z.object({
         moduleId: z.string().min(1),
       }),
     )
+    // This mutation function takes in the input object and deletes the entry in the database
     .mutation(async ({ ctx, input }) => {
       return await ctx.db.module.delete({
         where: {
@@ -56,3 +54,12 @@ export const moduleRouter = createTRPCRouter({
       });
     }),
 });
+
+// TODO: Remove or restrucure these comments later
+// This is a tRPC router. A router is a collection of various tRPC endpoints or 'procedures' as they call it.
+// We could technically have all our tRPC endpoints/procedures in a single router, but we use multiple routers for seperation of concerns.
+// So one router to manage API calls to Module table while another router manages the API calls to Attendance Record table
+// A procedure can be used to do 2 main things, either query some data or mutate some data somewhere
+
+// This here is a procedure used to query the database and return some data
+// The frontend can 'call' this API endpoint/procedure to fetch this data from backend
