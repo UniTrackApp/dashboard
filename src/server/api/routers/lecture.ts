4f92569d420a6608ca9e9df86a16c9800fe097ad
lewisj576 <84625224+lewisj576@users.noreply.github.com>
2023-11-22 20:29:39 +0000

    feat: Created tRPC procedures for Lecture router (#91)

--- src/server/api/routers/lecture.ts
+++ src/server/api/routers/lecture.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const lectureRouter = createTRPCRouter({
@@ -25,4 +26,48 @@ export const lectureRouter = createTRPCRouter({
       },
     });
   }),
+
+  createLectureRecord: protectedProcedure
+    .input(
+      z.object({
+        lectureId: z.string(),
+        startTime: z.date(),
+        endTime: z.date(),
+        moduleId: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const newLectureRecord = await ctx.db.lecture.create({
+          data: {
+            lectureId: input.lectureId,
+            startTime: input.startTime,
+            endTime: input.endTime,
+            moduleId: input.moduleId,
+          },
+        });
+
+        return newLectureRecord;
+      } catch (error) {
+        console.error("Error creating attendance record:", error);
+
+        throw new Error("Failed to create attendance record");
+      }
+    }),
+
+
+  deleteLectureRecordById: protectedProcedure
+  .input(
+    z.object({
+      lectureId: z.string(),
+    }),
+  )
+  .mutation(async ({ ctx, input }) => {
+    return await ctx.db.lecture.delete({
+      where: {
+        lectureId: input.lectureId,
+      },
+    });
+  }),
 });
+
