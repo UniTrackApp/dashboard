146bcfed74fb1a439c156ce168044ffab0582136
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-27 00:06:20 +0000

    feat: Implemented skeletal loading states (#112)
    Close #112

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -1,3 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { Separator } from "~/components/ui/separator";
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <Skeleton className="h-[30px] w-[200px]" />
+      <Skeleton className="h-[40px] w-[600px]" />
+      <Separator />
+      <Skeleton className="h-[500px] w-full" />
+    </div>
+  );
 }
