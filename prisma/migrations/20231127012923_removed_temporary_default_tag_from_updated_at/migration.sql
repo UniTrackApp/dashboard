469c4e926e0c120f80a925b05f72b38ca02754ad
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-27 01:33:18 +0000

    feat (db): Added timestamp fields to models (#51) Close #51

---
+++ prisma/migrations/20231127012923_removed_temporary_default_tag_from_updated_at/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "AttendanceRecord" ALTER COLUMN "updatedAt" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "Enrollment" ALTER COLUMN "updatedAt" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "Lecture" ALTER COLUMN "updatedAt" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "Module" ALTER COLUMN "updatedAt" DROP DEFAULT;
