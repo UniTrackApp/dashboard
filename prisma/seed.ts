e8c020690e3ae49418c007bd2c389fd581b96f0f
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-28 19:40:50 +0000

    style: Applied Prettier formatting to config files

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,226 +1,226 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { PrismaClient, Status } from "@prisma/client";
-const prisma = new PrismaClient();
+import { PrismaClient, Status } from '@prisma/client'
+const prisma = new PrismaClient()
 
 async function main() {
   // Students
   const student_aryan = await prisma.student.upsert({
     where: {
-      studentId: "77240000",
+      studentId: '77240000',
     },
     update: {},
     create: {
-      studentId: "77240000",
-      studentCardId: "ABC123",
-      firstName: "Aryan",
-      lastName: "Prince",
+      studentId: '77240000',
+      studentCardId: 'ABC123',
+      firstName: 'Aryan',
+      lastName: 'Prince',
     },
-  });
+  })
 
   const student_andrea = await prisma.student.upsert({
     where: {
-      studentId: "33590000",
+      studentId: '33590000',
     },
     update: {},
     create: {
-      studentId: "33590000",
-      studentCardId: "DEF456",
-      firstName: "Andrea",
-      lastName: "La Fauci De Leo",
+      studentId: '33590000',
+      studentCardId: 'DEF456',
+      firstName: 'Andrea',
+      lastName: 'La Fauci De Leo',
     },
-  });
+  })
 
   const student_lewis = await prisma.student.upsert({
     where: {
-      studentId: "33560000",
+      studentId: '33560000',
     },
     update: {},
     create: {
-      studentId: "33560000",
-      studentCardId: "GHI789",
-      firstName: "Lewis",
-      lastName: "Johnson",
+      studentId: '33560000',
+      studentCardId: 'GHI789',
+      firstName: 'Lewis',
+      lastName: 'Johnson',
     },
-  });
+  })
 
   // Modules
   const module_1 = await prisma.module.create({
     data: {
-      moduleId: "COMP766",
-      moduleName: "Smart Systems",
-      moduleDesc: "A dive into IoT and cloud computing",
+      moduleId: 'COMP766',
+      moduleName: 'Smart Systems',
+      moduleDesc: 'A dive into IoT and cloud computing',
     },
-  });
+  })
 
   const module_2 = await prisma.module.create({
     data: {
-      moduleId: "COMP723",
-      moduleName: "Software Engineering",
-      moduleDesc: "Learn how to build software with C#",
+      moduleId: 'COMP723',
+      moduleName: 'Software Engineering',
+      moduleDesc: 'Learn how to build software with C#',
     },
-  });
+  })
 
   const module_3 = await prisma.module.create({
     data: {
-      moduleId: "COMP738",
-      moduleName: "Network Management",
-      moduleDesc: "An advanced look into networking with SNMP",
+      moduleId: 'COMP738',
+      moduleName: 'Network Management',
+      moduleDesc: 'An advanced look into networking with SNMP',
     },
-  });
+  })
 
   // Lectures
   const lecture_1 = await prisma.lecture.create({
     data: {
-      lectureId: "COMP766-001",
-      moduleId: "COMP766",
-      startTime: new Date("2023-11-28T09:00:00.000Z"),
-      endTime: new Date("2023-11-28T11:00:00.000Z"),
+      lectureId: 'COMP766-001',
+      moduleId: 'COMP766',
+      startTime: new Date('2023-11-28T09:00:00.000Z'),
+      endTime: new Date('2023-11-28T11:00:00.000Z'),
     },
-  });
+  })
 
   const lecture_2 = await prisma.lecture.create({
     data: {
-      lectureId: "COMP723-005",
-      moduleId: "COMP723",
-      startTime: new Date("2023-11-24T10:00:00.000Z"),
-      endTime: new Date("2023-11-24T12:00:00.000Z"),
+      lectureId: 'COMP723-005',
+      moduleId: 'COMP723',
+      startTime: new Date('2023-11-24T10:00:00.000Z'),
+      endTime: new Date('2023-11-24T12:00:00.000Z'),
     },
-  });
+  })
 
   const lecture_3 = await prisma.lecture.create({
     data: {
-      lectureId: "COMP738-009",
-      moduleId: "COMP738",
-      startTime: new Date("2023-11-21T09:30:00.000Z"),
-      endTime: new Date("2023-11-21T11:00:00.000Z"),
+      lectureId: 'COMP738-009',
+      moduleId: 'COMP738',
+      startTime: new Date('2023-11-21T09:30:00.000Z'),
+      endTime: new Date('2023-11-21T11:00:00.000Z'),
     },
-  });
+  })
 
   const lecture_4 = await prisma.lecture.create({
     data: {
-      lectureId: "COMP766-031",
-      moduleId: "COMP766",
-      startTime: new Date("2023-11-27T13:00:00.000Z"),
-      endTime: new Date("2023-11-27T15:00:00.000Z"),
+      lectureId: 'COMP766-031',
+      moduleId: 'COMP766',
+      startTime: new Date('2023-11-27T13:00:00.000Z'),
+      endTime: new Date('2023-11-27T15:00:00.000Z'),
     },
-  });
+  })
 
   const lecture_5 = await prisma.lecture.create({
     data: {
-      lectureId: "COMP723-006",
-      moduleId: "COMP723",
-      startTime: new Date("2023-11-25T11:00:00.000Z"),
-      endTime: new Date("2023-11-25T12:30:00.000Z"),
+      lectureId: 'COMP723-006',
+      moduleId: 'COMP723',
+      startTime: new Date('2023-11-25T11:00:00.000Z'),
+      endTime: new Date('2023-11-25T12:30:00.000Z'),
     },
-  });
+  })
 
   // Enrollments
   const enrollment_1 = await prisma.enrollment.create({
     data: {
-      studentId: "77240000",
-      moduleId: "COMP766",
+      studentId: '77240000',
+      moduleId: 'COMP766',
     },
-  });
+  })
 
   const enrollment_2 = await prisma.enrollment.create({
     data: {
-      studentId: "77240000",
-      moduleId: "COMP723",
+      studentId: '77240000',
+      moduleId: 'COMP723',
     },
-  });
+  })
 
   const enrollment_3 = await prisma.enrollment.create({
     data: {
-      studentId: "33590000",
-      moduleId: "COMP723",
+      studentId: '33590000',
+      moduleId: 'COMP723',
     },
-  });
+  })
 
   const enrollment_4 = await prisma.enrollment.create({
     data: {
-      studentId: "33560000",
-      moduleId: "COMP738",
+      studentId: '33560000',
+      moduleId: 'COMP738',
     },
-  });
+  })
 
   const enrollment_5 = await prisma.enrollment.create({
     data: {
-      studentId: "33560000",
-      moduleId: "COMP766",
+      studentId: '33560000',
+      moduleId: 'COMP766',
     },
-  });
+  })
 
   // Attendance Records
   const attendanceRecord_1 = await prisma.attendanceRecord.create({
     data: {
-      studentId: "77240000",
-      lectureId: "COMP766-001",
+      studentId: '77240000',
+      lectureId: 'COMP766-001',
       status: Status.PRESENT,
-      timestamp: new Date("2023-11-28T09:03:24.000Z"),
+      timestamp: new Date('2023-11-28T09:03:24.000Z'),
     },
-  });
+  })
 
   const attendanceRecord_2 = await prisma.attendanceRecord.create({
     data: {
-      studentId: "33590000",
-      lectureId: "COMP766-001",
+      studentId: '33590000',
+      lectureId: 'COMP766-001',
       status: Status.LATE,
-      timestamp: new Date("2023-11-24T09:21:42.000Z"),
+      timestamp: new Date('2023-11-24T09:21:42.000Z'),
     },
-  });
+  })
 
   const attendanceRecord_3 = await prisma.attendanceRecord.create({
     data: {
-      studentId: "33560000",
-      lectureId: "COMP766-001",
+      studentId: '33560000',
+      lectureId: 'COMP766-001',
       status: Status.ABSENT,
-      timestamp: new Date("2023-11-28T11:00:00.000Z"),
+      timestamp: new Date('2023-11-28T11:00:00.000Z'),
     },
-  });
+  })
 
   const attendanceRecord_4 = await prisma.attendanceRecord.create({
     data: {
-      studentId: "77240000",
-      lectureId: "COMP723-005",
+      studentId: '77240000',
+      lectureId: 'COMP723-005',
       status: Status.LATE,
-      timestamp: new Date("2023-11-24T10:24:31.000Z"),
+      timestamp: new Date('2023-11-24T10:24:31.000Z'),
     },
-  });
+  })
 
   const attendanceRecord_5 = await prisma.attendanceRecord.create({
     data: {
-      studentId: "33590000",
-      lectureId: "COMP766-031",
+      studentId: '33590000',
+      lectureId: 'COMP766-031',
       status: Status.PRESENT,
-      timestamp: new Date("2023-11-24T13:01:24.000Z"),
+      timestamp: new Date('2023-11-24T13:01:24.000Z'),
     },
-  });
+  })
 
   const attendanceRecord_6 = await prisma.attendanceRecord.create({
     data: {
-      studentId: "33560000",
-      lectureId: "COMP723-006",
+      studentId: '33560000',
+      lectureId: 'COMP723-006',
       status: Status.PRESENT,
-      timestamp: new Date("2023-11-27T11:02:34.000Z"),
+      timestamp: new Date('2023-11-27T11:02:34.000Z'),
     },
-  });
+  })
 
   const attendanceRecord_7 = await prisma.attendanceRecord.create({
     data: {
-      studentId: "77240000",
-      lectureId: "COMP738-009",
+      studentId: '77240000',
+      lectureId: 'COMP738-009',
       status: Status.PRESENT,
-      timestamp: new Date("2023-11-21T09:41:05.000Z"),
+      timestamp: new Date('2023-11-21T09:41:05.000Z'),
     },
-  });
+  })
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect();
+    await prisma.$disconnect()
   })
   .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,165 +1,165 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}", // Tremor module
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}', // Tremor module
   ],
   theme: {
-    transparent: "transparent",
-    current: "currentColor",
+    transparent: 'transparent',
+    current: 'currentColor',
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
       colors: {
         // shadcn-ui colors
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
         },
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
         },
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
         },
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
         },
         // tremor.so colors - light mode
         tremor: {
           brand: {
-            faint: "#eff6ff", // blue-50
-            muted: "#bfdbfe", // blue-200
-            subtle: "#60a5fa", // blue-400
-            DEFAULT: "#3b82f6", // blue-500
-            emphasis: "#1d4ed8", // blue-700
-            inverted: "#ffffff", // white
+            faint: '#eff6ff', // blue-50
+            muted: '#bfdbfe', // blue-200
+            subtle: '#60a5fa', // blue-400
+            DEFAULT: '#3b82f6', // blue-500
+            emphasis: '#1d4ed8', // blue-700
+            inverted: '#ffffff', // white
           },
           background: {
-            muted: "#fafafa", // neutral-50
-            subtle: "#f5f5f5", // neutral-100
-            DEFAULT: "#ffffff", // white
-            emphasis: "#404040", // neutral-700
+            muted: '#fafafa', // neutral-50
+            subtle: '#f5f5f5', // neutral-100
+            DEFAULT: '#ffffff', // white
+            emphasis: '#404040', // neutral-700
           },
           border: {
-            DEFAULT: "#e5e5e5", // neutral-200
+            DEFAULT: '#e5e5e5', // neutral-200
           },
           ring: {
-            DEFAULT: "#e5e5e5", // neutral-200
+            DEFAULT: '#e5e5e5', // neutral-200
           },
           content: {
-            subtle: "#a3a3a3", // neutral-400
-            DEFAULT: "#737373", // neutral-500
-            emphasis: "#404040", // neutral-700
-            strong: "#171717", // neutral-900
-            inverted: "#ffffff", // white
+            subtle: '#a3a3a3', // neutral-400
+            DEFAULT: '#737373', // neutral-500
+            emphasis: '#404040', // neutral-700
+            strong: '#171717', // neutral-900
+            inverted: '#ffffff', // white
           },
         },
         // tremor.so colors - dark mode
-        "dark-tremor": {
+        'dark-tremor': {
           brand: {
-            faint: "#0B1229", // custom
-            muted: "#172554", // blue-950
-            subtle: "#1e40af", // blue-800
-            DEFAULT: "#3b82f6", // blue-500
-            emphasis: "#60a5fa", // blue-400
-            inverted: "#0a0a0a", // neutral-950
+            faint: '#0B1229', // custom
+            muted: '#172554', // blue-950
+            subtle: '#1e40af', // blue-800
+            DEFAULT: '#3b82f6', // blue-500
+            emphasis: '#60a5fa', // blue-400
+            inverted: '#0a0a0a', // neutral-950
           },
           background: {
-            muted: "#131A2B", // custom
-            subtle: "#262626", // neutral-800
-            DEFAULT: "#0a0a0a", // neutral-900
-            emphasis: "#d4d4d4", // neutral-300
+            muted: '#131A2B', // custom
+            subtle: '#262626', // neutral-800
+            DEFAULT: '#0a0a0a', // neutral-900
+            emphasis: '#d4d4d4', // neutral-300
           },
           border: {
-            DEFAULT: "#262626", // neutral-800
+            DEFAULT: '#262626', // neutral-800
           },
           ring: {
-            DEFAULT: "#262626", // neutral-800
+            DEFAULT: '#262626', // neutral-800
           },
           content: {
-            subtle: "#525252", // neutral-600
-            DEFAULT: "#737373", // neutral-500
-            emphasis: "#e5e5e5", // neutral-200
-            strong: "#fafafa", // neutral-50
-            inverted: "#000000", // black
+            subtle: '#525252', // neutral-600
+            DEFAULT: '#737373', // neutral-500
+            emphasis: '#e5e5e5', // neutral-200
+            strong: '#fafafa', // neutral-50
+            inverted: '#000000', // black
           },
         },
       },
       // shadcn-ui - Other customizations
       borderRadius: {
         // shadcn-ui
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
         // tremor.so
-        "tremor-small": "0.375rem",
-        "tremor-default": "0.5rem",
-        "tremor-full": "9999px",
+        'tremor-small': '0.375rem',
+        'tremor-default': '0.5rem',
+        'tremor-full': '9999px',
       },
       keyframes: {
-        "accordion-down": {
+        'accordion-down': {
           from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: 0 },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
       // tremor.so - Other customizations
       boxShadow: {
         // light
-        "tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
-        "tremor-card":
-          "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-        "tremor-dropdown":
-          "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+        'tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+        'tremor-card':
+          '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+        'tremor-dropdown':
+          '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
         // dark
-        "dark-tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
-        "dark-tremor-card":
-          "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-        "dark-tremor-dropdown":
-          "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+        'dark-tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+        'dark-tremor-card':
+          '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+        'dark-tremor-dropdown':
+          '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
       },
       fontSize: {
-        "tremor-label": ["0.75rem"],
-        "tremor-default": ["0.875rem", { lineHeight: "1.25rem" }],
-        "tremor-title": ["1.125rem", { lineHeight: "1.75rem" }],
-        "tremor-metric": ["1.875rem", { lineHeight: "2.25rem" }],
+        'tremor-label': ['0.75rem'],
+        'tremor-default': ['0.875rem', { lineHeight: '1.25rem' }],
+        'tremor-title': ['1.125rem', { lineHeight: '1.75rem' }],
+        'tremor-metric': ['1.875rem', { lineHeight: '2.25rem' }],
       },
     },
   },
@@ -167,17 +167,17 @@ module.exports = {
     {
       pattern:
         /^(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ["hover", "ui-selected"],
+      variants: ['hover', 'ui-selected'],
     },
     {
       pattern:
         /^(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ["hover", "ui-selected"],
+      variants: ['hover', 'ui-selected'],
     },
     {
       pattern:
         /^(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ["hover", "ui-selected"],
+      variants: ['hover', 'ui-selected'],
     },
     {
       pattern:
@@ -192,5 +192,5 @@ module.exports = {
         /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
     },
   ],
-  plugins: [require("tailwindcss-animate")],
-};
+  plugins: [require('tailwindcss-animate')],
+}
