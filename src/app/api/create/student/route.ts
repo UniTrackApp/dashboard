a0c9e4f5d460e84f67091d4fa16114187a19e89d
A. La Fauci <87176210+Bosurgi@users.noreply.github.com>
2023-11-29 12:27:29 +0000

    feat: API route to create a student (#90)

--- src/app/api/create/student/route.ts
+++ src/app/api/create/student/route.ts
@@ -1,7 +1,47 @@
 // TODO: Andrea, remove this ESLint exception when you start working on this file
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
+import { z } from 'zod'
+import { prisma } from '~/server/prisma'
+
+
+type StudentInfo = {
+  studentId: string
+  studentCardId: string
+  firstName: string
+  lastName: string
+}
+
 export async function POST(request: Request) {
-  const res = await request.json()
-  return Response.json({ res })
+  const { studentCardId, studentId, firstName, lastName } =
+    (await request.json()) as StudentInfo
+
+  // Error checking if parameters are missing
+  if (!studentCardId || !studentId || !firstName || !lastName) {
+    return Response.json(
+      { error: 'Missing parameters in request body' },
+      {
+        status: 400,
+      },
+    )
+  }
+
+  // Creates a student with the parameters received from the request body
+  const result = await prisma.student.create({
+    data: {
+      studentCardId: studentCardId,
+      studentId: studentId,
+      firstName: firstName,
+      lastName: lastName,
+    },
+  })
+
+  // Returns a message if student successfully created and the student object
+  return Response.json(
+    {
+      message: 'Student created successfully',
+      student: result,
+    },
+    { status: 200 },
+  )
 }
