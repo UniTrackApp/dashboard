e6582ce6fa56879020fc85328cfc7b8311efc92c
Aryan Prince <aryanprincesanty@icloud.com>
2023-12-01 00:53:46 +0000

    chore: Updated view options button

--- src/components/ui/data-table/view-options.tsx
+++ src/components/ui/data-table/view-options.tsx
@@ -23,11 +23,7 @@ export function DataTableViewOptions<TData>({
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          size="sm"
-          className="ml-auto hidden h-8 lg:flex"
-        >
+        <Button variant="outline" className="ml-auto hidden lg:flex">
           <Settings2 className="mr-2 h-4 w-4" />
           Edit View
         </Button>
