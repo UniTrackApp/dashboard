db9b3798f3a9ecae78c4d1076c8820e1b4d9bd3a
Aryan Prince <aryanprincesanty@icloud.com>
2023-12-06 18:21:21 +0300

    chore: Created a sleep util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+// Function to merge Tailwind classes with clsx, useful for conditional CSS classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
@@ -8,4 +9,12 @@ export function cn(...inputs: ClassValue[]) {
 // Function to get the first name from a full name, used across the app
 export const getFirstName = (fullName: string | null | undefined) => {
   return fullName?.split(' ')[0]
+}
+
+// Function to create a timeout promise, used to test loading states
+export const sleep = (seconds: number) => {
+  const SECONDS_TO_MILLISECONDS_FACTOR = 1000
+  return new Promise((resolve) =>
+    setTimeout(resolve, seconds * SECONDS_TO_MILLISECONDS_FACTOR),
+  )
 }
